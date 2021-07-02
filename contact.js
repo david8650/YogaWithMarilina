e1fd6ec9c6f9a6a3c237882d01421cddb48a19eb
david8650 <davidmdsvalentim@gmail.com>
2021-07-02 16:16:09 +0100

    Added Formspree Form for Contact

--- contact.js
+++ contact.js
@@ -13,9 +13,5 @@ clearBtn.addEventListener('click', ()=>{
     message.value = "";
 });
 
-var formvalidator  = new Validator("contactform");
 
-formvalidator.addValidation("name","req","Please provide your name");
-formvalidator.addValidation("email","req","Please provide your email");
-formvalidator.addValidation("email","email","Please enter a valid email address");
 
